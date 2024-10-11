28078bf7de3582ffd7bcb3eef2d10bd14820d118
Nickolay68/LessonOB02 <p.nick21vek@gmail.com>
2024-10-11 17:06:05 +0300

    add time

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+@app.route('/')
+@app.route("/<name>")
+def home():
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return f"<h1>Current Date and Time</h1><p>{current_time}</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
